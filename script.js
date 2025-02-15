03a09c2568a0ba457b7316fdbefda1323e02c5b6
DrewMiner <andwmi@gmail.com>
2025-02-15 03:04:25 -0600

    Added FAQ section with expandable questions and added links to header and footer menus for blog and faq

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Slideshow
 const slides = document.querySelectorAll(".hero-slideshow img");
 let currentSlide = 0;
 
@@ -8,3 +9,11 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 4000); // Change image every 4 seconds
+
+// FAQ Expandable 
+document.querySelectorAll(".faq-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const answer = button.nextElementSibling;
+        answer.style.display = answer.style.display === "block" ? "none" : "block";
+    });
+});
